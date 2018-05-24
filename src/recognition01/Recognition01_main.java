297c9824bfa48b869367000e029823a947f265dd
J16016 <J16016@J16016-PC>
2018-05-24 12:49:00 +0900

    second commit

--- src/recognition01/Recognition01_main.java
+++ src/recognition01/Recognition01_main.java
@@ -2,7 +2,10 @@ package recognition01;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectFacesOptions;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectedFaces;
@@ -24,6 +27,36 @@ public class Recognition01_main {
 		}
 		DetectedFaces result = service.detectFaces(detectFacesOptions).execute();
 		System.out.println(result);
+
+		String s = String.valueOf(result);
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		try {
+			JsonNode node = mapper.readTree(s);
+			int age_min = node.get("images").get(0).get("faces").get(0).get("age").get("min").asInt();
+			 System.out.println("age_min : " + age_min);
+
+			 int age_max = node.get("images").get(0).get("faces").get(0).get("age").get("max").asInt();
+			 System.out.println("age_max : " + age_max);
+
+			 float age_score = node.get("images").get(0).get("faces").get(0).get("age").get("score").floatValue();
+			 System.out.println("age_score : " + age_score);
+
+			 String gender = node.get("images").get(0).get("faces").get(0).get("gender").get("gender").asText();
+			 System.out.println("gender : " + gender);
+
+			 float gender_min = node.get("images").get(0).get("faces").get(0).get("gender").get("score").floatValue();
+			 System.out.println("gender_min : " + gender_min);
+
+
+
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+
 
 	}
 
